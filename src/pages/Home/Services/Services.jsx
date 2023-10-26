f893afefff32e2a01823788752be571179cf3781
Tahsin1111 <entimojumder@gmail.com>
2023-10-26 23:14:33 +0600

    Create a simple Login page with firebase integration

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -20,7 +20,7 @@ const Services = () => {
                  </p>
 
             </div>
-            <div className="gird grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {
                 services.map(service => <ServiceCard
                 key={service._id}
