9ea190b907c8bdfe12d49f836e1c4636df331b6a
Tahsin1111 <entimojumder@gmail.com>
2023-10-31 19:34:34 +0600

    project done

--- src/pages/Bookings/BookingRow.jsx
+++ src/pages/Bookings/BookingRow.jsx
@@ -1,14 +1,15 @@
 
 
-const BookingRow = ({booking}) => {
-    const {customerName,email,date,service,price,img} =booking;
+const BookingRow = ({booking,handleDelete,handleBookingConfirm}) => {
+    
+    const {_id,customerName,email,date,service,price,img,status} =booking;
     return (
-
+ 
       <tr>
       <th>
-        <label>
-          <input type="checkbox" className="checkbox" />
-        </label>
+      <button onClick={()=>handleDelete(_id)} className="btn btn-sm btn-circle btn-outline">
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+</button>
       </th>
       <td>
      
@@ -28,7 +29,9 @@ const BookingRow = ({booking}) => {
       <td>{date}</td>
       <td>${price}</td>
       <th>
-        <button className="btn btn-ghost btn-xs">details</button>
+        { status === 'confirm' ? <span className="font-bold text-primary">COnfirmed</span> :
+          <button onClick={()=>handleBookingConfirm(_id)} className="btn btn-ghost btn-xs"> please confirm</button>
+        }
       </th>
     </tr>
     );
